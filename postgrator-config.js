2e5bddb7b561fa2e4355b9de7ef79d861839c156
Hyunwoo Moon <mhw9163@gmail.com>
2021-03-01 14:42:02 -0800

    heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-undef */
 require('dotenv').config();
 
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production"
+? {rejectUnauthorized : false}
+: false;
+
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "connectionString" : (process.NODE_ENV === 'test')
+                      ? process.env.TEST_DATABASE_URL
+                      : process.env.DATABASE_URL, 
 }
